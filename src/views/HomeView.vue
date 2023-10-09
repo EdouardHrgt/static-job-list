de9803247952909f1cec8534e04f16e3e0c015be
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2023-10-09 14:54:28 +0200

    Wip global layout & responsive + search by tag feature

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,27 +2,82 @@
 import jobs from '../datas/data.json';
 import { ref, computed } from 'vue';
 
-const jobList = ref(jobs);
-
 function getImageUrl(name) {
   return new URL(`../assets/${name}`, import.meta.url).href;
 }
+
+function setFeatured(bool) {
+  if (bool) return 'featured';
+}
+
+function setNew(bool) {
+  if (bool) return 'new!';
+}
+
+const jobList = ref(jobs);
+const tagSet = ref(new Set());
+const tagsList = computed(() => {
+  return [...tagSet.value];
+});
+
+function addToTags(tag) {
+  tagSet.value.add(tag);
+}
+
+const tagsListBis = computed(() => {
+  const selectedTags = [...tagSet.value]; // Convertir les tagsSet en tableau
+  if (selectedTags.length === 0) {
+    return jobList.value; // Si aucun tag n'est sélectionné, renvoyer la liste complète de jobs
+  } else {
+    // Filtrer les jobs qui contiennent au moins un des tags sélectionnés
+    return jobList.value.filter((job) => {
+      // Vérifier si au moins un tag sélectionné est présent dans les langages, les outils, le rôle ou le niveau du job
+      return selectedTags.some((tag) => {
+        return job.languages.includes(tag) || job.tools.includes(tag) || job.role === tag || job.level === tag;
+      });
+    });
+  }
+});
 </script>
 
 <template>
-  <header></header>
-  <main class="">
-    <section class="flex" v-for="job in jobList" :key="job.id">
-      <img :src="getImageUrl(job.logo)" alt="" />
-      <div class="card-txt">
-        <p>
-          {{ job.company }} <span>{{ job.new }}</span> <span>{{ job.new }}</span>
+  <header>
+    <h1 v-if="tagsList.length" class="text-xl font-bold">{{ tagsListBis }}</h1>
+  </header>
+  <main class="bg-teal-50 py-20 px-6 lg:px-6">
+    <section
+      class="relative rounded-md md:flex max-w-6xl mx-auto md:my-10 mb-16 md:mb-0 items-center md:gap-5 lg:gap-8 px-8 py-8 bg-white border-l-[5px] border-teal-500 shadow-xl"
+      v-for="job in jobList"
+      :key="job.id"
+    >
+      <img :src="getImageUrl(job.logo)" alt="" class="absolute md:static top-[-2rem] w-16 md:w-auto" />
+      <div class="card-txt mt-5 md:mt-0">
+        <p class="font-bold text-teal-500">
+          {{ job.company }}
+          <span v-if="job.new" class="uppercase text-sm text-white bg-teal-500 px-3 py-1 rounded-full mx-2">{{
+            setNew(job.new)
+          }}</span>
+          <span v-if="job.featured" class="uppercase text-sm text-white bg-slate-800 px-3 py-1 rounded-full mx-1">{{
+            setFeatured(job.featured)
+          }}</span>
         </p>
-        <p>{{ job.postedAt }} - {{ job.contract }} - {{ job.location }}</p>
-        <ul class="flex">
-          <li v-for="lang in job.languages" :key="lang">{{ lang }}</li>
-        </ul>
+        <h2 class="text-xl font-bold my-1">{{ job.position }}</h2>
+        <p class="text-gray-400 border-b-2 pb-2 md:border-0">{{ job.postedAt }} - {{ job.contract }} - {{ job.location }}</p>
       </div>
+      <ul class="flex gap-3 ml-auto mt-4 md:mt-0 flex-wrap">
+        <li class="bg-teal-50 px-2 py-1 font-bold text-teal-500" v-for="lang in job.languages" :key="lang">
+          <button @click="addToTags(lang)">{{ lang }}</button>
+        </li>
+        <li class="bg-teal-50 px-2 py-1 font-bold text-teal-500" v-for="tool in job.tools" :key="tool">
+          <button @click="addToTags(tool)">{{ tool }}</button>
+        </li>
+        <li class="bg-teal-50 px-2 py-1 font-bold text-teal-500">
+          <button @click="addToTags(job.role)">{{ job.role }}</button>
+        </li>
+        <li class="bg-teal-50 px-2 py-1 font-bold text-teal-500">
+          <button @click="addToTags(job.level)">{{ job.level }}</button>
+        </li>
+      </ul>
     </section>
   </main>
 </template>
@@ -38,14 +93,6 @@ header {
   height: 10rem;
 }
 
-main {
-  max-width: 1250px;
-  margin-inline: auto;
-}
-
 section {
-  margin-block: 2rem;
-  border: 1px solid lightseagreen;
-  padding: 2rem 0rem;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        cShadow: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+      },
+    },
   },
   plugins: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
